da3c3234c95c926b5c466e12ef7ecc421291c932
l0st15 <daniilhramov970@gmail.com>
2024-02-21 23:13:35 +0700

    добавлен ввод имени

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
+#include<string>
 
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello word\n";
+    string name;
+    cout << "Enter your name\n";
+    cin >> name;
+
+    cout << "Hello word from " << name << "\n";
 
     return 0;
 }
